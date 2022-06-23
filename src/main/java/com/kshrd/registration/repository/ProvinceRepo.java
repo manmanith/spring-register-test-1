3a4d32acbec54cc035c144e975e852fd21d7101e
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2022-06-23 22:46:10 +0700

    get all province

--- src/main/java/com/kshrd/registration/repository/ProvinceRepo.java
+++ src/main/java/com/kshrd/registration/repository/ProvinceRepo.java
@@ -4,8 +4,13 @@ import com.kshrd.registration.model.Province;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface ProvinceRepo {
     @Select("SELECT * FROM province WHERE id = #{proId}")
     Province getProvinceById(Integer proId);
+
+    @Select("SELECT * FROM province")
+    List<Province> getAllProvince();
 }
