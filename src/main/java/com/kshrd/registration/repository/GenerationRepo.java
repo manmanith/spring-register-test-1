542c115c434c7c787d654443b3a089da2af29fb9
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2022-06-24 16:21:42 +0700

    apply generation

--- src/main/java/com/kshrd/registration/repository/GenerationRepo.java
+++ src/main/java/com/kshrd/registration/repository/GenerationRepo.java
@@ -1,12 +1,22 @@
 package com.kshrd.registration.repository;
 
+import com.kshrd.registration.exception.ListTypeHandler;
 import com.kshrd.registration.model.Generation;
 import com.kshrd.registration.model.Province;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface GenerationRepo {
     @Select("SELECT * FROM generation WHERE id = #{genId}")
     Generation getGenerationById(Integer genId);
+
+    @Select("SELECT * FROM generation WHERE status = true")
+    @Results(id = "generationResults", value = {
+            @Result(property = "startDate", column = "start_date"),
+            @Result(property = "endDate", column = "end_date"),
+            @Result(property = "delayDay", column = "delay_day"),
+            @Result(property = "examUrl", column = "exam_url", typeHandler = ListTypeHandler.class),
+            @Result(property = "timelineUrl", column = "timeline_url"),
+    })
+    Generation getOpenGeneration();
 }
