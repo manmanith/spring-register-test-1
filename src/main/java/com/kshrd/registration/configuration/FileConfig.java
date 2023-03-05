b95043dc2e15d83caecad2dcb76bc82b19381591
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2023-03-05 13:47:52 +0700

    add base

--- src/main/java/com/kshrd/registration/configuration/FileConfig.java
+++ src/main/java/com/kshrd/registration/configuration/FileConfig.java
@@ -9,7 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class FileConfig implements WebMvcConfigurer {
 
     //Be careful: "src/main/resources/images" error without slash;
-    String serverPath = "/app/src/main/resources/images/";
+    //String serverPath = "/app/src/main/resources/images/";
+    String serverPath = "/src/main/resources/images/";
 //    String serverPath = "/home/leangsengk90/images/";
 
     @Value("${file.upload.server.path}")
