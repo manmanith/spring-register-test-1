0ee43bf18a8163aec19e53bea0e0f7bb6f197ed4
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-03-19 12:59:26 +0700

    Update deploy.sh

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -5,35 +5,44 @@ cat << EOF > /home/dara/kubernetes/register-app.yml
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: register-deploy
+ name: register-deployment
 spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: register-app
-  template:
-    metadata:
-      labels:
+ replicas: 2
+ selector:
+   matchLabels:
+     app: register-app
+ template:
+   metadata:
+     labels:
        app: register-app
-    spec:
-      containers:
-        - name: register-cont
-          image: xeng/register-k8s:$1
-          ports:
-            - containerPort: 8080
+   spec:
+     nodeSelector:
+       group: workers
+     containers:
+       - name: register-cont
+         image: xeng/spring-register:$1
+         ports:
+           - containerPort: 8080
+         volumeMounts:
+           - name: register-vol
+             mountPath: /src/main/resources/images
+     volumes:
+         - name: register-vol
+           nfs:
+             server: 10.148.0.12
+             path: /opt/nfs/data
 ---
 apiVersion: v1
 kind: Service
 metadata:
-  name: register-svc
+ name: register-svc
 spec:
-  type: NodePort
-  selector:
-    app: register-app
-  ports:
-    - port: 8080
-      targetPort: 8080
-      nodePort: 38080
+ type: NodePort
+ selector:
+   app: register-app
+ ports:
+   - port: 8080
+     nodePort: 32222
       
 EOF
 
