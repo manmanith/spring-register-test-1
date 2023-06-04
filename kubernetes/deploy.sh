1ea6b266981f39ab45db4063581d864eae46dedc
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-06-04 16:06:30 +0700

    Update deploy.sh

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,65 +1,39 @@
 #!/bin/bash
 
-cat << EOF > /home/dara/kubernetes/register-app.yml
-
+cat << EOF > /opt/register-deploy.yml
 apiVersion: apps/v1
 kind: Deployment
 metadata:
- name: register-deployment
+  name: register-deploy
 spec:
- replicas: 2
- selector:
-   matchLabels:
-     app: register-app
- template:
-   metadata:
-     labels:
-       app: register-app
-   spec:
-     containers:
-       - name: register-cont
-         image: xeng/spring-register:$1
-         ports:
-           - containerPort: 8080
-         volumeMounts:
-           - name: register-vol
-             mountPath: /src/main/resources/images
-     volumes:
-         - name: register-vol
-           nfs:
-             server: 10.148.0.12
-             path: /opt/nfs/data
+  replicas: 3
+  selector:
+    matchLabels:
+      app: register-app
+  template: 
+    metadata:
+      labels:
+        app: register-app
+    spec:
+      containers:
+        - name: register-cont
+          image: xeng/kube-register:1.1.1
+          ports:
+            - containerPort: 8080
+
 ---
 apiVersion: v1
 kind: Service
 metadata:
- name: register-svc
-spec:
- type: ClusterIP
- selector:
-   app: register-app
- ports:
-   - port: 8080
----
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: nginx-ingress
-  annotations:
-    nginx.ingress.kubernetes.io/rewrite-target: /\$2
-spec:
-  rules:
-    - host: register.e-crops.co
-      http:
-        paths:
-          - path: /spring(/|$)(.*)
-            pathType: Prefix
-            backend:
-              service:
-                name: register-svc
-                port:
-                  number: 8080
+  name: register-svc
+spec: 
+  type: NodePort
+  selector:
+    app: register-app
+  ports:
+    - port: 8080
+      targetPort: 8080
+      nodePort: 30003
 EOF
 
-# Deploy with kubernetes
-kubectl apply -f /home/dara/kubernetes/register-app.yml
+kubectl apply -f /opt/register-deploy.yml
